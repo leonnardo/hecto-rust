741c4ce64f207f3111f8b4a315d421bd1ce5a820
Leonnardo Rabello <leonnardo@gmail.com>
2023-09-02 08:46:19 +0000

    Draw welcome message

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -1,6 +1,7 @@
 use crate::Terminal;
 use termion::event::Key;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
@@ -51,14 +52,28 @@ impl Editor {
         Ok(())
     }
 
-    fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height - 1 {
-            Terminal::clear_current_line();
-            println!("~\r");
-        }
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message)
     }
 
-
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+            println!("~\r");
+            }
+        }
+    }
 }
 
 fn die(e: std::io::Error) {
